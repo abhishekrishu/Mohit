4bd8713bad4638132fd10a5963c2aebd738837bb
abhishek singh <riahu.abhishek40@gmail.com>
2016-11-17 15:22:20 +0530

    sir why i have to make one more java class here

---
+++ app/src/main/java/com/example/android/mohit/Mohitt.java
@@ -0,0 +1,16 @@
+package com.example.android.mohit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by android on 11/17/2016.
+ */
+
+ class Mohitt extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Mohitt);
+    }
+}
